30664560d8a293e34f3c9f905bd50eaf154c482b
isai <Isai4827@4827@gmail.com>
2019-03-28 13:19:05 -0700

    ...

--- app/src/components/pages/search/index.js
+++ app/src/components/pages/search/index.js
@@ -92,15 +92,15 @@ function search() {
               onClick={() => {
                 Axios.get("/api/users/search/" + parks[parkData]).then(({ data }) => {
                   // set state and create if statement that loops through the alerts (if it has any), and if not leave blank & only display the rest of the info
-                  console.log(data.parks.data);
-                  console.log(data.parks.data[0].directionsinfo);
-                  console.log(data.parks.data[0].weatherinfo);
-                  console.log(data.parks.data[0].fullname);
-                  console.log(data.alerts.data[0].category)
-                  console.log(data.alerts.data[0].title)
-                  console.log(data.alerts.data[1].title);
-                  console.log(data.alerts.data[2].title);
-                  setPark("");
+                  if(data.alerts.data === []){
+                    console.log("nothing to show")
+                  }else{
+                    // console.log(data.alerts.data[0].title);
+                    // console.log(data.alerts.data[1].title);
+                    data.alert.data.map(alert => alert.title);
+                  }
+                  // setUserParks([...userParks, data]);
+                  // setPark("");
                 })
               }}
               style={{ marginLeft: "10px" }}
