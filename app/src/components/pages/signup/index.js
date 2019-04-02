e4bf72610858adab2fe65fd2b6102227dfe26ac0
SLawless <slawless05@gmail.com>
2019-04-02 05:46:01 -0700

    minor profile changes

--- app/src/components/pages/signup/index.js
+++ app/src/components/pages/signup/index.js
@@ -65,6 +65,7 @@ const styles = theme => ({
   avatar: {
     margin: theme.spacing.unit,
     backgroundColor: theme.palette.secondary.main,
+    marginTop: theme.spacing.unit * 6,
   },
   form: {
     width: '100%', // Fix IE 11 issue.
@@ -72,7 +73,6 @@ const styles = theme => ({
   },
   submit: {
     marginTop: theme.spacing.unit * 3,
-    color: theme.palette.secondary.main,
     backgroundColor: theme.palette.primary.main,
   },
 });
